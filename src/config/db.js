d7ed97ef3724a4217e304e5a90226f8672e113f7
Juanguppy <86721752+Juanguppy@users.noreply.github.com>
2025-03-22 15:08:41 +0100

    MongoDB connection created & tested

---
+++ src/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DB_CONNECTION);
+    console.log(`MongoDB conectado: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error al conectar a MongoDB: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
