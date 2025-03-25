57e8fcad2f97c033e832a13b2add350842fbe596
AlexBenedi <843826@unizar.es>
2025-03-25 00:15:39 +0000

    getProfile nad register implementation

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,15 @@
+import passport from "passport";
+import "./passport.js";
+
+import { register } from "../controllers/users.js";
+import { login } from "../controllers/auth.js";
+import { getProfile } from "../controllers/users.js";
+
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post('/register', register);
+router.post('/login', login);
+router.get('/profile', passport.authenticate('jwt', { session: false }), getProfile);
 
 module.exports = router;
