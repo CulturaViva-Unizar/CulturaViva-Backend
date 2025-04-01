6d8dfba9624e94a56f219e7f3ebcff7085372a64
AlexBenedi <843826@unizar.es>
2025-04-01 00:27:02 +0000

    Add saved-events and assisted-events. Not tested

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,10 +2,6 @@ const UserModel = require("../models/userModel");
 
 class UserController {
 
-  constructor() {
-    this.userModel = new UserModel();
-  }
-
   /**
    * Obtiene el perfil del usuario
    */
@@ -71,15 +67,22 @@ class UserController {
    */
   async getSavedItems(req, res) {
     try {
-      const userId = req.userId;
+      const { name, date, category } = req.query;
 
+      const userId = req.userId;
       const page = parseInt(req.query.page) || 1;
-      const limit = parseInt(req.query.limit) || 16;  
+      const limit = parseInt(req.query.limit) || 16;
+
+      const filters = {};
+      if (name) filters.name = name
+      if (date) filters.date = date
+      if (category) filters.category = category
       
       const total = await UserModel.findById(userId).select('savedItems').countDocuments();
       const user = await UserModel.findById(userId)
       .populate({
         path: 'savedItems',
+        match: filters,
         options: {
           limit: limit,
           skip: skip
@@ -104,37 +107,164 @@ class UserController {
       });
     }
   }
-  
-  /**
-   * Actualiza un elemento específico del usuario
-   */
-  async changeUserElement(userId, element, value) {
-    try {    
 
-      const updateData = { [field]: value };
-      const updatedUser = await UserModel.findByIdAndUpdate(
-        userId,
-        updateData,
-        { new: true, runValidators: true }
-      );
+  /**
+   * Guarda un evento en el perfil del usuario
+   */
+  async saveItem(req, res) {
+    const userId = req.userId;
+    const { eventId } = req.body;
+
+    try {
+      const user = await UserModel.findById(userId);
+
+      user.savedItems.push(eventId);
+      await user.save();
 
       return res.status(200).json({
         success: true,
-        message: "Campo actualizado exitosamente",
-        data: updatedUser
+        message: "Evento guardado exitosamente",
+        data: user.savedItems
+      });
+    } catch (error) {
+      console.error("Error al guardar el evento:", error);
+      return res.status(500).json({
+        success: false,
+        message: "Error interno del servidor al guardar el evento",
+      });
+    }
+  }
+
+  /**
+   * Obtiene los eventos a los que el usuario asiste
+   */
+  async getAttendingItems(req, res) {
+
+    const { name, date, category } = req.query;
+
+    const userId = req.userId;
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 16;
+
+    const filters = {};
+    if (name) filters.name = name
+    if (date) filters.date = date
+    if (category) filters.category = category
+
+    try {
+      const total = await UserModel.findById(userId).select('asistsTo').countDocuments();
+      const user = await UserModel.findById(userId)
+      .populate({
+        path: 'asistsTo',
+        match: filters,
+        options: {
+          limit: limit,
+          skip: skip
+        }
+        
+      });
+
+      return res.status(200).json({
+        success: true,
+        data: user.asistsTo,
+        pagination: {
+          currentPage: page,
+          totalPages: Math.ceil(total / limit),
+          totalItems: total
+        }
       });
 
     } catch (error) {
-      console.error("Error al actualizar un elemento del perfil:", error);
+      console.error("Error al obtener los eventos a los que asiste:", error);
       return res.status(500).json({
         success: false,
-        message: "Error interno del servidor al actualizar un elemento del perfil",
+        message: "Error interno del servidor al obtener los eventos a los que asiste",
+      });
+    }
+  }
+
+  /**
+   * Marca como asistiente a un evento
+   */
+  async attendItem(req, res) {
+    const userId = req.userId;
+    const { eventId } = req.body;
+
+    try {
+      const user = await UserModel.findById(userId);
+
+      user.asistsTo.push(eventId);
+      await user.save();
+
+      return res.status(200).json({
+        success: true,
+        message: "Evento marcado como asistente exitosamente",
+        data: user.attendingItems
+      });
+    }
+    catch (error) {
+      console.error("Error al marcar el evento como asistente:", error);
+      return res.status(500).json({
+        success: false,
+        message: "Error interno del servidor al marcar el evento como asistente",
+      });
+    }
+  }
+
+  /**
+   * Elimina un evento guardado del perfil del usuario
+   */
+  async removeSavedItem(req, res) {
+    const userId = req.userId;
+    const { eventId } = req.params;
+
+    try {
+      const user = await UserModel.findById(userId);
+
+      user.savedItems = user.savedItems.filter(item => item.toString() !== eventId);
+      await user.save();
+
+      return res.status(200).json({
+        success: true,
+        message: "Evento eliminado de los guardados exitosamente",
+        data: user.savedItems
+      });
+    } catch (error) {
+      console.error("Error al eliminar el evento guardado:", error);
+      return res.status(500).json({
+        success: false,
+        message: "Error interno del servidor al eliminar el evento guardado",
+      });
+    }
+  }
+
+  /**
+   * Elimina un evento al que el usuario asiste
+   */
+  async removeAttendingItem(req, res) {
+    const userId = req.userId;
+    const { eventId } = req.params;
+
+    try {
+      const user = await UserModel.findById(userId);
+      user.asistsTo = user.asistsTo.filter(item => item.toString() !== eventId);
+      await user.save();
+      return res.status(200).json({
+        success: true,
+        message: "Evento eliminado de los asistidos exitosamente",
+        data: user.attendingItems
+      });
+    }
+    catch (error) {
+      console.error("Error al eliminar el evento asistido:", error);
+      return res.status(500).json({
+        success: false,
+        message: "Error interno del servidor al eliminar el evento asistido",
       });
     }
   }
 }
 
-// Exportar una única instancia del controlador
 module.exports = new UserController();
 
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -7,9 +7,17 @@ const userController = require('../controllers/userController');
 const router = express.Router();
 
 router.get('/me', passport.authenticate('jwt', { session: false }), userController.getProfile);
+router.get('/me/saved-events', passport.authenticate('jwt', { session: false }), userController.getSavedItems);
+router.get('me/attending-events', passport.authenticate('jwt', { session: false }), userController.getAttendingItems);
 
 router.put('/me', passport.authenticate('jwt', { session: false }), userController.updateProfile);
 
-router.get('/me/saved', passport.authenticate('jwt', { session: false }), userController.getSavedItems);
+router.post('/me/saved-events', passport.authenticate('jwt', { session: false }), userController.saveItem);
+router.post('/me/attending-events', passport.authenticate('jwt', { session: false }), userController.attendItem);
+
+router.delete('/me/saved-events/:eventId', passport.authenticate('jwt', { session: false }), userController.removeSavedItem);
+router.delete('/me/attending-events/:eventId', passport.authenticate('jwt', { session: false }), userController.removeAttendingItem);
+
+
 
 module.exports = router;
