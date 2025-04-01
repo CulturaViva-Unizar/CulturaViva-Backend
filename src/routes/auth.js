f74dd4d63f447ed6894f075506869fd56c0bff6d
AlexBenedi <843826@unizar.es>
2025-04-01 00:25:37 +0000

    Add JWT authentication to change-password route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,9 +2,12 @@ const express = require('express');
 require('../config/passport');
 const authController = require('../controllers/authController');
 const router = express.Router();
+const passport = require('passport');
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
-router.post('/change-password', authController.changePassword);
+router.post('/change-password', 
+    passport.authenticate('jwt', { session: false }), 
+    authController.changePassword);
 
 module.exports = router;
