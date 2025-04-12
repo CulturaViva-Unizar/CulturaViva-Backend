bdb7b49d369ae62a898727f8bbe14961a56f6c45
Juanguppy <86721752+Juanguppy@users.noreply.github.com>
2025-04-12 15:14:20 +0200

    Validate json is not working, temporaly deleted

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ var app = express();
 
 db.connectDB();
 
-app.use(validateJson);
+//app.use(validateJson);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
