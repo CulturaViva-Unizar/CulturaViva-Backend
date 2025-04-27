3112c6aa3669a8b0e6f51f554e89ecae1d816203
AlexBenedi <843826@unizar.es>
2025-04-27 22:40:21 +0000

    Update file env

--- src/config/env.js
+++ src/config/env.js
@@ -4,8 +4,8 @@ const { env } = process;
 
 const config = {
     DB_CONNECTION: env.DB_CONNECTION,
-    JWT_SECRET: env.JWT_SECRET || 'secret',
-    JWT_EXPIRES: env.JWT_EXPIRES || '1d',
+    JWT_SECRET: env.JWT_SECRET,
+    JWT_EXPIRES: env.JWT_EXPIRES,
     PORT: env.PORT,
     GOOGLE_CLIENT_ID: env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: env.GOOGLE_CLIENT_SECRET,
