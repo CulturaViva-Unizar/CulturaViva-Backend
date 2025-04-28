455de4d20affb08bab2302da296056e3adb666a8
AlexBenedi <843826@unizar.es>
2025-04-28 19:37:39 +0000

    Add comments

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -5,7 +5,7 @@ const userSchema = new Schema({
   name: {
     type: String,
     required: true
-    //unique: true  Para permiti
+    //unique: true  Para permitir multiples tipos de cuenta con el mismo email
   },
   email: {
     type: String,
