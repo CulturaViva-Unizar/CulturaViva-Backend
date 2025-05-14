5451a5c491650f013bc39917c0ddf46a90b2535c
AlexBenedi <843826@unizar.es>
2025-05-14 00:56:28 +0000

    Middleware to count visits

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+const statisticsController = require('../controllers/statisticsController');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', statisticsController.countVisits, function(req, res, next) {
   res.send('Hello World');
 });
 
