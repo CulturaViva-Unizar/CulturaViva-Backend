f9c780b14ee4e0a320b712b30d8955491ae40b2b
AlexBenedi <843826@unizar.es>
2025-05-21 19:18:15 +0000

    Config jest

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   testMatch: ['**/tests/**/*.test.js'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/app.js',
+    '!src/config/**',
+    '!src/public/**',
+  ],
+  coverageReporters: ['text', 'lcov', 'clover', 'html'],
 };
